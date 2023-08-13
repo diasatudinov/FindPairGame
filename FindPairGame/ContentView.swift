1e24643adad0672508a9e1ed2e52f34799b5d01d
Dias <dias.atudinov@gmail.com>
2023-08-13 12:07:09 +0600

    initial commit

---
+++ FindPairGame/ContentView.swift
@@ -0,0 +1,25 @@
+//  ContentView.swift
+//  FindPairGame
+//
+//  Created by Atudinov Dias on 13.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FindPairGame/FindPairGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindPairGameApp.swift
+//  FindPairGame
+//
+//  Created by Atudinov Dias on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct FindPairGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
